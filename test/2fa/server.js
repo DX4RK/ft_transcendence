b574de81add423f74d96a4bbc759cc9a51636698
root <root@LAPTOP-K0F61PFJ.localdomain>
2025-09-25 22:47:37 +0200

    normalement la 2fa fonctionne

--- test/2fa/server.js
+++ test/2fa/server.js
@@ -7,8 +7,8 @@ const {Vonage} = require('@vonage/server-sdk');
 const speakeasy = require('speakeasy');
 const qrcode = require('qrcode');
 
-const PORT = 3000;
 const app = express();
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -29,25 +29,36 @@ const code_tmp_2fa = new Map();
 //  Login
 //----------------
 
-  // sign in
+  // sign up
 
-app.post('/signIn', async (req, res) => {
+app.post('/signUp', async (req, res) => {
   const { login, password } = req.body;
 
   if (clients_password.has(login)) return res.status(400).json({ success: false, message: 'Utilisateur déjà inscrit' });
 
   clients_password.set(login, password);
 
+  console.log("sign up");
+  console.log(login);
+  console.log(password);
+
   res.json({ success: true });
 });
 
-  // sign up
+  // sign in
 
-app.post('/signUp', async (req, res) => {
+app.post('/signIn', async (req, res) => {
   const { login, password } = req.body;
   const expected_password = clients_password.get(login);
   const method = clients_method_2fa.get(login);
 
+  console.log("sign in");
+  console.log(method);
+  console.log(expected_password);
+  console.log(login);
+  console.log(password);
+
+  if (!clients_password.has(login)) return res.status(400).json({ success: false, message: 'Utilisateur inconu' });
   if (password !== expected_password) return res.status(401).json({ success: false, message: 'Mot de pass incorect' });
   
   try {
@@ -109,6 +120,8 @@ app.post('/signUp', async (req, res) => {
 
 app.post('/sms/send', async (req, res) => {
   const { login, phone } = req.body;
+
+  if (!login) return res.status(400).json({ success:false, message:'Login requis' });
   if (!phone) return res.status(400).json({ success:false, message:'Téléphone requis' });
 
   const code = Math.floor(100000 + Math.random() * 900000);
@@ -133,18 +146,28 @@ app.post('/sms/send', async (req, res) => {
 
 app.post('/sms/verify', (req, res) => {
   const { login, phone, code } = req.body;
-  const expectedCode = code_tmp_2fa.get(phone);
+
+  if (!login) return res.status(400).json({ success: false, message: 'Login requis' });
+  if (!code) return res.status(400).json({ success: false, message: 'Code requis' });
+
+  const userPhone = phone || clients_phone.get(login);
+
+  if (!userPhone) return res.status(400).json({ success: false, message: 'Numero requis' });
+
+  const expectedCode = code_tmp_2fa.get(userPhone);
 
   if (!expectedCode) return res.json({ success: false, message: 'Pas de secret pour cet utilisateur' });
 
 
   if (Number(code) === expectedCode) {
-    code_tmp_2fa.delete(phone);
+    code_tmp_2fa.delete(userPhone);
 
-    const token = jwt.sign({ userLogin: login, userPhone: phone }, /*process.env.JWT_SECRET*/'c4d33a89a49710c12fce5a7d272f470a82ddd04b7680f46529514fce5ad41905', { expiresIn: '7d' });
+    const token = jwt.sign({ userLogin: login, userPhone: userPhone }, /*process.env.JWT_SECRET*/'c4d33a89a49710c12fce5a7d272f470a82ddd04b7680f46529514fce5ad41905', { expiresIn: '7d' });
    
     clients_method_2fa.set(login, "sms");
-    clients_phone.set(login, phone);
+    clients_phone.set(login, userPhone);
+    
+    console.log(clients_method_2fa.get(login));
 
     return res.json({ success: true, token });
   }
@@ -157,6 +180,8 @@ app.post('/sms/verify', (req, res) => {
 
 app.post('/email/send', async (req, res) => {
   const { login, email } = req.body;
+
+  if (!login) return res.status(400).json({ success:false, message:'Login requis' });
   if (!email) return res.status(400).json({ success:false, message:'Email requis' });
 
   const code = Math.floor(100000 + Math.random() * 900000);
@@ -191,17 +216,25 @@ app.post('/email/send', async (req, res) => {
 
 app.post('/email/verify', (req, res) => {
   const { login, email, code } = req.body;
-  const expectedCode = code_tmp_2fa.get(email);
+
+  if (!login) return res.status(400).json({ success: false, message: 'Login requis' });
+  if (!code) return res.status(400).json({ success: false, message: 'Code requis' });
+
+  const userEmail = email || clients_email.get(login);
+
+  if (!userEmail) return res.status(400).json({ success: false, message: 'Email requis' });
+
+  const expectedCode = code_tmp_2fa.get(userEmail);
 
   if (!expectedCode) return res.json({ success: false, message: 'Pas de secret pour cet utilisateur' });
 
   if (Number(code) === expectedCode) {
-    code_tmp_2fa.delete(email);
+    code_tmp_2fa.delete(userEmail);
 
-    const token = jwt.sign({ userLogin: login, userEmail: email }, /*process.env.JWT_SECRET*/'c4d33a89a49710c12fce5a7d272f470a82ddd04b7680f46529514fce5ad41905', { expiresIn: '7d' });
+    const token = jwt.sign({ userLogin: login, userEmail: userEmail }, /*process.env.JWT_SECRET*/'c4d33a89a49710c12fce5a7d272f470a82ddd04b7680f46529514fce5ad41905', { expiresIn: '7d' });
 
     clients_method_2fa.set(login, "email");
-    clients_email.set(login, email);
+    clients_email.set(login, userEmail);
 
     return res.json({ success: true, token });
   }
@@ -215,6 +248,7 @@ app.post('/email/verify', (req, res) => {
 
 app.post('/totp/send', async (req, res) => {
   const { login } = req.body;
+
   if (!login) return res.status(400).json({ success: false, message: 'Login requis' });
 
   try {
@@ -242,6 +276,10 @@ app.post('/totp/send', async (req, res) => {
 
 app.post('/totp/verify', (req, res) => {
   const { login, code } = req.body;
+
+  if (!login) return res.status(400).json({ success: false, message: 'Login requis' });
+  if (!code) return res.status(400).json({ success: false, message: 'Code requis' });
+
   const expectedCode = code_tmp_2fa.get(login);
 
   if (!expectedCode) return res.json({ success: false, message: 'Pas de secret pour cet utilisateur' });
@@ -257,6 +295,8 @@ app.post('/totp/verify', (req, res) => {
     const jwtToken = jwt.sign({ userLogin: login }, /*process.env.JWT_SECRET*/'c4d33a89a49710c12fce5a7d272f470a82ddd04b7680f46529514fce5ad41905', { expiresIn: '7d' });
     
     clients_method_2fa.set(login, "totp");
+    
+    console.log(clients_method_2fa.get(login));
 
     return res.json({ success: true, token: jwtToken });
   }
