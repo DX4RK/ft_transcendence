2c682d9d63a81adbb4c8c1e28c11f5d0031f0f49
nolan <firesocietygames@gmail.com>
2025-10-08 16:33:56 +0200

    Added: sign in api

--- src/containers/fastify/src/routes/sign/sign.js
+++ src/containers/fastify/src/routes/sign/sign.js
@@ -35,7 +35,35 @@ async function signRoutes(fastify, opts) {
 	});
 
 	fastify.post('/in', async (request, reply) => {
-		
+		const { username, password } = request.body;
+
+		if (!username || !password) {
+			return reply.code(400).send({ success: false, message: 'Missing username, or password' });
+		}
+
+		try {
+			const stmt = fastify.usersDb.prepare('SELECT * FROM users WHERE username = ?');
+			const user = stmt.get(username);
+
+			if (!user) {
+				return reply.code(400).send({ success: false, message: 'User not found' });
+			}
+
+			const passwordMatch = await bcrypt.compare(password, user.password_hash);
+
+			if (!passwordMatch) {
+				return reply.code(401).send({ success: false, message: 'Invalid password' });
+			}
+
+			return reply.send({
+				success: true,
+				message: 'Login successfull',
+				userId: user.id,
+			});
+		} catch (err) {
+			fastify.log.error(err);
+			return reply.code(500).send({ success: false, message: 'Server error' });
+		}
 	});
 };
 
