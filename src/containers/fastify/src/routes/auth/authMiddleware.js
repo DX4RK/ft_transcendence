cda3bd21a8639cca4042996daa272bd94c55bcf5
nolan <firesocietygames@gmail.com>
2025-10-11 18:09:21 +0200

    Changed: function to fastify route module

--- src/containers/fastify/src/routes/auth/authMiddleware.js
+++ src/containers/fastify/src/routes/auth/authMiddleware.js
@@ -1,20 +1,45 @@
 const { verifyToken } = require("../../service/jwt");
 
-async function authMiddleware(request, reply) {
-  const authHeader = request.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+async function authMiddleware2(request, reply) {
+	const authHeader = request.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) {
-    return reply.status(401).json({ success: false, message: 'Token manquant' });
-  }
+	if (!token) {
+		return reply.status(401).json({ success: false, message: 'Missing token.' });
+	}
 
-  const { valid, decoded } = verifyToken(token);
+	const { valid, decoded } = verifyToken(token);
 
-  if (!valid) {
-    return reply.status(403).json({ success: false, message: "Token invalide" });
-  }
+	if (!valid) {
+		return reply.status(403).json({ success: false, message: "Invalid token." });
+	}
 
-  request.user = decoded;
+	request.user = decoded;
 };
+
+async function authMiddleware(fastify, opts) {
+	const { verifyToken } = opts;
+
+	fastify.post('/test', async (request, reply) => {
+		const authHeader = request.headers['authorization'];
+		const token = authHeader && authHeader.split(' ')[1];
+console.log(authHeader);
+		if (!token) {
+			return reply.code(401).send({ success: false, message: 'Missing token' });
+		}
+
+		const { valid, decoded } = verifyToken(token);
+
+		if (!valid) {
+			return reply.code(403).send({ success: false, message: 'Invalid token' });
+		}
+
+		return reply.send({
+			success: true,
+			message: 'Access granted.',
+			user: decoded,
+		});
+	});
+}
 
 module.exports = authMiddleware;
