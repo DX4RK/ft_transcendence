b989af0fc63113a5ca55f5d4354864f6e67c668c
nolan <firesocietygames@gmail.com>
2025-10-24 10:47:42 +0200

    Changed: react added livechat page

--- src/containers/nginx/front/src/App.tsx
+++ src/containers/nginx/front/src/App.tsx
@@ -1,31 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "@/components/theme-provider"
-import { io } from "socket.io-client";
+import { SocketProvider } from "@/contexts/SocketContext"
 
 import Home from "@/pages/Home"
 import LiveChat from "@/pages/LiveChat"
 import SignIn from "@/pages/login/page"
 import SignUp from "@/pages/signup/page"
 
-const socket = io("http://localhost:3000", {
-    withCredentials: true
-});
-
-socket.on('message', () => {
-	console.log("hello");
-});
-
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/live" element={<LiveChat />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <SocketProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/live" element={<LiveChat />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </SocketProvider>
     </ThemeProvider>
   )
 }
