bdda9067b434cbdbf3092c59da22009e21ff15ec
nolan <firesocietygames@gmail.com>
2025-10-24 11:07:46 +0200

    Updated: send sms service

--- src/containers/fastify/src/service/send/send_sms.js
+++ src/containers/fastify/src/service/send/send_sms.js
@@ -1,17 +1,11 @@
-async function sendSMSCode(vonage, login, phone, code_tmp_2fa) {
+async function sendSMSCode(vonage, user, phone, code) {
+	const info = await vonage.sms.send({
+		to: phone,
+		from: 'ft_transcendence',
+		text: `Hello ${user}, your verification code is ${code}`
+	})
 
-    const code = Math.floor(100000 + Math.random() * 900000);
-
-    const response = await vonage.sms.send({
-        to: phone,
-        from: '2FA-Test',
-        text: `Bonjour ${login}, votre code de vérification est : ${code}`
-    });
-
-    code_tmp_2fa.set(phone, code);
-    setTimeout(() => code_tmp_2fa.delete(phone), 5 * 60 * 1000);
-
-    return response;
+	return code;
 }
 
 module.exports = sendSMSCode;
