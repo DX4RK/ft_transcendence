add174ebc0d702a41efe6aead3476566222c1f12
nolan <firesocietygames@gmail.com>
2025-10-30 15:50:07 +0100

    Added: getSettings rotue handling

--- src/containers/fastify/src/routes/index.js
+++ src/containers/fastify/src/routes/index.js
@@ -8,6 +8,8 @@ const updateStatsOnMatchFinishRoute = require('./stats/updateStatsOnMatchFinish'
 
 const authMiddlewareRoute = require('./auth/authMiddleware');
 
+const getSettings = require('./my/getSettings');
+
 const socketAuthHandlers = require('../plugins/socket-handlers/auth.js');
 const socketChatHandlers = require('../plugins/socket-handlers/chat.js');
 const socketNotificationHandlers = require('../plugins/socket-handlers/notifications.js');
@@ -17,9 +19,11 @@ const registerRoutes = async (fastify, { transporter, vonage, generateToken, ver
 	await fastify.register(authMiddlewareRoute, { verifyToken });
 
 	// Stats
-
 	await fastify.register(getMatchStatsRoute, { prefix: '/stats', verifyToken });
 	await fastify.register(updateStatsOnMatchFinishRoute, { prefix: '/stats', verifyToken });
+
+	// My
+	await fastify.register(getSettings, { prefix: '/my', verifyToken });
 
 	// Check
 	fastify.get('/', async (request, reply) => {
