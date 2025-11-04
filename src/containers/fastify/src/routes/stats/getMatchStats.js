7263fe6aaf7d751b61281b1f0cc76ab3b1a558f4
nolan <noldianes@gmail.com>
2025-11-04 02:48:09 +0100

    Added: xp return

--- src/containers/fastify/src/routes/stats/getMatchStats.js
+++ src/containers/fastify/src/routes/stats/getMatchStats.js
@@ -5,18 +5,22 @@ async function getMatchStats(fastify, opts) {
 		async (request, reply) => {
 			const decoded = request.user;
 
-			const stmt = fastify.usersDb.prepare('SELECT * FROM user_matches WHERE user_id = ?');
+			let stmt = fastify.usersDb.prepare('SELECT * FROM user_matches WHERE user_id = ?');
 			const userStats = stmt.get(decoded.userId);
+
+			stmt = fastify.usersDb.prepare('SELECT * FROM users WHERE id = ?');
+			const userData = stmt.get(decoded.userId);
 
 			if (!userStats) {
 				return reply.send({
 					success: true,
 					message: 'No stats yet.',
 					data: {
-						history: {},
+						xp: userData.experience_point,
 						matchWon: 0,
 						matchLost: 0,
 						matchPlayed: 0,
+						history: {},
 					}
 				});
 			}
@@ -32,6 +36,7 @@ async function getMatchStats(fastify, opts) {
 				success: true,
 				message: 'User stats retrieved successfully.',
 				data: {
+					xp: userData.experience_point,
 					matchWon: userStats.match_won,
 					matchLost: (userStats.match_played - userStats.match_won),
 					matchPlayed: userStats.match_played,
