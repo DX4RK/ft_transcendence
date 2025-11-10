447908aaff8ab0b6f47bf61567faf2735bd56d41
nolan <firesocietygames@gmail.com>
2025-11-10 14:15:25 +0100

    fixed send sms

--- src/containers/fastify/src/service/send/send_sms.js
+++ src/containers/fastify/src/service/send/send_sms.js
@@ -1,9 +1,12 @@
 async function sendSMSCode(vonage, user, phone, code) {
-	const info = await vonage.sms.send({
-		to: phone,
-		from: 'Vonage',
-		text: `Hello ${user}, your verification code is ${code}`
-	})
+
+	const from = "Transcendence";
+	const to = phone;
+	const text = `Hello ${user}, your verification code is ${code}`;
+
+	await vonage.sms.send({to, from, text})
+		.then(resp => { console.log('Message sent successfully'); console.log(resp); })
+		.catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
 
 	return code;
 }
